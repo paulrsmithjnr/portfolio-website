c28bc4a35c4ca33c51cc3cc5c484d0cc48c1c38e
Paul Smith <55596699+paulrsmithjnr@users.noreply.github.com>
2024-10-02 13:05:18 -0500

    created technologies section component

---
+++ src/components/Technologies.tsx
@@ -0,0 +1,31 @@
+import { technologies } from "../constants";
+import { SectionWrapper } from "./hoc";
+import BallCanvas from "./canvas/Ball";
+import SectionHeader from "./SectionHeader";
+import { useRemoteConfig } from "./RemoteConfigComponent";
+
+const Technologies = () => {
+  const configValues = useRemoteConfig();
+  return (
+    <div className="py-20 w-full">
+      <SectionHeader
+        text={configValues.technologiesSectionHeading as string}
+        highlightCount={4}
+      />
+
+      <div className="mt-20 flex flex-row flex-wrap justify-center gap-10">
+        {technologies.map((technology) => (
+          <div
+            className="w-32 h-32 flex flex-col items-center"
+            key={technology.name}
+          >
+            <BallCanvas icon={technology.icon} />
+            <span className="text-white text-center">{technology.name}</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SectionWrapper(Technologies, "technologies");
