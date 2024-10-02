7f83220d5a9269315ac10d69c5fcfadb97c45be9
Paul Smith <55596699+paulrsmithjnr@users.noreply.github.com>
2024-10-02 12:36:07 -0500

    assets folder

---
+++ src/assets/index.tsx
@@ -0,0 +1,23 @@
+export { default as wink } from "./wink.png";
+export { default as me } from "./me.png";
+export { default as codebg } from "./codebg.svg";
+export { default as uiElement } from "./uiElement.svg";
+export { default as grid } from "./grid.svg";
+export { default as computerBg } from "./computerBg.png";
+
+export { default as typescript } from "./tech/typescript.png";
+export { default as reactjs } from "./tech/reactjs.png";
+export { default as tailwind } from "./tech/tailwind.png";
+export { default as firebase } from "./tech/firebase.svg";
+export { default as python } from "./tech/python.png";
+export { default as java } from "./tech/java.png";
+export { default as flutter } from "./tech/flutter.png";
+
+export { default as mgi } from "./company/mgi.png";
+export { default as speur } from "./company/speur.png";
+export { default as ncb } from "./company/ncb.png";
+export { default as gkfg } from "./company/gkfg.png";
+
+export { default as linkedin } from "./linkedin.svg";
+export { default as github } from "./github.svg";
+export { default as footerGrid } from "./footerGrid.svg";
