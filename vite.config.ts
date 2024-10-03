a18f06f46b37fa26df4eeae08b94fd4f6d49b5dc
Paul Smith <55596699+paulrsmithjnr@users.noreply.github.com>
2024-10-03 12:51:42 -0500

    prevent browser caching

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,11 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    headers: {
+      'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0',
+      'Pragma': 'no-cache',
+      'Expires': '0',
+    },
+  },
 })
