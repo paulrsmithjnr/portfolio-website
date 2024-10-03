611614f6bf0219991036db2d9b19bf202ac2e978
Paul Smith <55596699+paulrsmithjnr@users.noreply.github.com>
2024-10-03 01:39:21 -0500

    ui adjustments

--- src/components/ui/FloatingNav.tsx
+++ src/components/ui/FloatingNav.tsx
@@ -51,7 +51,7 @@ export const FloatingNav: React.FC<FloatingNavProps> = ({
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit md:min-w-[70vw] lg:min-w-fit fixed z-[5000] top-10 inset-x-0 mx-auto px-10 py-5 rounded-lg border border-black/.1 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] items-center justify-center space-x-4",
+          "flex max-w-fit md:min-w-[70vw] lg:min-w-fit fixed z-[5000] top-10 inset-x-0 mx-auto px-10 py-5 rounded-full border bg-black-100 border-white/[0.2] shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] items-center justify-center space-x-4",
           className
         )}
         style={{
@@ -66,7 +66,7 @@ export const FloatingNav: React.FC<FloatingNavProps> = ({
             key={`link=${idx}`}
             href={navItem.link}
             className={cn(
-              "relative dark:text-neutral-50 items-center  flex space-x-1 text-neutral-50 dark:hover:text-neutral-300 hover:text-neutral-300"
+              "relative items-center  flex space-x-1 text-neutral-50 hover:text-neutral-300"
             )}
           >
             <span className="block sm:hidden">{navItem.icon}</span>
